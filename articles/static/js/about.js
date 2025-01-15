f1e653d25713b154c980e1760d030ff61e68ba81
Mik-Halo <sprone3@gmail.com>
2025-01-15 18:27:10 +0200

    add responsive styles

---
+++ articles/static/js/about.js
@@ -0,0 +1,9 @@
+const expPara = document.getElementById('years-of-exp');
+const startDate = new Date('2022-05-01');
+
+function calcYearsOfExperience() {
+  const currYears = Math.floor((Date.now() - startDate.getTime()) / (1000 * 60 * 60 * 24 * 365.25));
+  expPara.textContent = currYears;
+}
+
+calcYearsOfExperience();
